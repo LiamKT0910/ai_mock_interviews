6620469f850071f4ced3af48b422a11c1b3499ba
LiamKT0910 <bigboy09101994@gmail.com>
2025-03-25 13:01:17 -0500

    implement interview generation ui

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -10,6 +10,12 @@ import Link from 'next/link';
 import { toast } from 'sonner';
 import FormField from '@/components/FormField';
 import { useRouter } from 'next/navigation';
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+} from 'firebase/auth';
+import { auth } from '@/firebase/client';
+import { signIn, signUp } from '@/lib/actions/auth.action';
 
 const authFormSchema = (type: FormType) => {
     return z.object({
@@ -32,20 +38,54 @@ const AuthForm = ({ type }: { type: FormType }) => {
     });
 
     // 2. Define a submit handler.
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    async function onSubmit(values: z.infer<typeof formSchema>) {
         try {
             if (type === 'sign-up') {
-                toast.success(
-                    'Account created successfully. You can sign in now.'
+                const { name, email, password } = values;
+                const userCredentials = await createUserWithEmailAndPassword(
+                    auth,
+                    email,
+                    password
                 );
+                const result = await signUp({
+                    uid: userCredentials.user.uid,
+                    name: name!,
+                    email,
+                    password,
+                });
+
+                if (!result?.success) {
+                    toast.error(result?.message);
+                    return;
+                }
+
+                toast.success('Account created successfully. Please sign in.');
                 router.push('/sign-in');
             } else {
+                const { email, password } = values;
+                const userCredential = await signInWithEmailAndPassword(
+                    auth,
+                    email,
+                    password
+                );
+
+                const idToken = await userCredential.user.getIdToken();
+                if (!idToken) {
+                    toast.error('Sign In failed');
+                    return;
+                }
+
+                await signIn({
+                    email,
+                    idToken,
+                });
+
                 toast.success('Sign in successfully.');
                 router.push('/');
             }
         } catch (error) {
-            console.error(error);
-            toast.error(`There was an error creating auth form: ${error}`);
+            console.log(error);
+            toast.error(`There was an error: ${error}`);
         }
     }
     const isSignIn = type === 'sign-in';
@@ -76,6 +116,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
                             name="email"
                             label="Email"
                             placeholder="Your Email Address"
+                            type="email"
                         />
                         <FormField
                             control={form.control}
---
+++ firebase/client.ts
@@ -0,0 +1,20 @@
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: 'AIzaSyDp-uL75bokCor-x3DXtSVe0TKGKzZlKvU',
+    authDomain: 'prepwise-c2797.firebaseapp.com',
+    projectId: 'prepwise-c2797',
+    storageBucket: 'prepwise-c2797.firebasestorage.app',
+    messagingSenderId: '1032527433440',
+    appId: '1:1032527433440:web:a2ffcbfad44b352639ca85',
+    measurementId: 'G-67FYJ47X2Q',
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
